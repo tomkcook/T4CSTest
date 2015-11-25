853c5a4523ff08e948c894fbc13f0d5e4523c41a
Tom Cook <tom.k.cook@gmail.com>
2015-11-25 12:03:59 +0000

    Initial project to demonstrate problem with assembly loading in T4 templates

---
+++ T4CSTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace T4CSTest
+{
+	public class Utilities
+	{
+		public string GetFirstNamespace(string code)
+		{
+			var tree = CSharpSyntaxTree.ParseText (code);
+			var root = tree.GetRoot ();
+			var @using = root.DescendantNodes ().OfType<UsingDirectiveSyntax> ();
+			var @types = root.DescendantNodes ().OfType<TypeDeclarationSyntax> ();
+			var @enums = root.DescendantNodes ().OfType<EnumDeclarationSyntax> ();
+			var @namespaces = root.DescendantNodes ().OfType<NamespaceDeclarationSyntax> ();
+			return @namespaces.First ().GetText ().ToString();
+		}
+	}
+}
---
+++ Templates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using T4CSTest;
+
+namespace Templates
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string code = "using System; namespace Foo { public class Program { public static void Main(string[] args) { Console.WriteLine(\"Hello, World\"); } } }";
+			Utilities u = new Utilities();
+			string ns = u.GetFirstNamespace(code);
+			Console.WriteLine (ns);
+		}
+	}
+}
+
